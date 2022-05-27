5affb93497b58a566c1f414cd58264774d71252b
Seynabou12 <dioneseynabou0@gmail.com>
2022-05-27 10:15:12 +0000

    Creation de la classe  Utilisateur et de l'interface UtilsateurRepository

---
+++ src/main/java/com/example/crud/utilisateur/Utilisateur.java
@@ -0,0 +1,75 @@
+package com.example.crud.utilisateur;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "utilisateurs")
+public class Utilisateur {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false , name = "penom" , length = 45)
+    private String prenom;
+
+    @Column(nullable = false, name = "nom" , length = 50)
+    private String nom;
+
+    @Column(nullable = false, length = 70, name = "email", unique = true)
+    private String email;
+
+    @Column(name = "telephone", length = 50, nullable = false)
+    private Integer telephone;
+
+    @Column(name = "password", length = 50, nullable = false)
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(Integer telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ src/main/java/com/example/crud/utilisateur/UtilsateurRepository.java
@@ -0,0 +1,8 @@
+package com.example.crud.utilisateur;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UtilsateurRepository extends CrudRepository<Utilisateur , Integer>{
+
+
+}
