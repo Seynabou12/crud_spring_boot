cc79d473625aad25f6de00526240ba790536ae50
Seynabou12 <dioneseynabou0@gmail.com>
2022-05-27 11:05:55 +0000

    gestion de la selection d'un utilisateur , la suppression et la modification d'un utilisateur

--- src/test/java/com/example/crud/UtilisateurRepositoryTest.java
+++ src/test/java/com/example/crud/UtilisateurRepositoryTest.java
@@ -2,12 +2,13 @@ package com.example.crud;
 
 import com.example.crud.utilisateur.Utilisateur;
 import com.example.crud.utilisateur.UtilsateurRepository;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.annotation.Rollback;
+
+import java.util.Optional;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
@@ -32,10 +33,35 @@ public class UtilisateurRepositoryTest {
     @Test
     public void testListAll(){
         Iterable<Utilisateur> utilisateurs = repo.findAll();
-
         for (Utilisateur utilisateur : utilisateurs)
         {
             System.out.println(utilisateur);
         }
+    }
+
+    @Test
+    public void testUpdate(){
+        int userId = 1;
+        Optional<Utilisateur> optionalUtilisateur = repo.findById(userId);
+        Utilisateur utilisateur = optionalUtilisateur.get();
+        utilisateur.setTelephone(768767654);
+        repo.save(utilisateur);
+        Utilisateur updateUtisateur = repo.findById(userId).get();
+    }
+
+    @Test
+    public void testGet(){
+        int userId = 2;
+        Optional<Utilisateur> optionalUtilisateur = repo.findById(userId);
+        Utilisateur utilisateur = optionalUtilisateur.get();
+        System.out.println(optionalUtilisateur.get());
+    }
+
+    @Test
+    public void testDelete(){
+        int userId = 4;
+        repo.deleteById(userId);
+        Optional<Utilisateur> optionalUtilisateur = repo.findById(userId);
+        System.out.println("utilisateur supprimer avec succés");
     }
 }
